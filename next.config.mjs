2a688dd006ce15dba3069af9c30e727952e943db
tissueMO <ts32worker@gmail.com>
2024-03-06 16:33:57 +0000

    GitHub Pagesへの自動デプロイに対応

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // basePath: '/##BASE_PATH##',
+  // images: { unoptimized: true },
 };
 
 export default nextConfig;
