19af637ad7fae685942f02190b8cdbb09e76c202
tissueMO <ts32worker@gmail.com>
2024-03-06 16:08:40 +0000

    GitHub Pagesへの自動デプロイに対応

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,13 +1,25 @@
 'use client';
 
+import config from '@/next.config.mjs';
 import { css } from '@/styled-system/css';
 import Image from 'next/image';
+
+const BASE_PATH = config.basePath ?? '';
 
 /**
  * ロゴ
  */
 export const Logo = () => {
-  return <Image src="/logo.svg" alt="Minesweeper Logo" width={180} height={37} priority className={logoStyle} />;
+  return (
+    <Image
+      src={`${BASE_PATH}/logo.svg`}
+      alt="Minesweeper Logo"
+      width={180}
+      height={37}
+      priority
+      className={logoStyle}
+    />
+  );
 };
 
 const logoStyle = css({
